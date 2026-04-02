1d9b5368984a77ba51b02daa5e5c6a3a2a00ab13
Alexis Graham <tul31142@temple.edu>
2026-04-02 11:48:52 -0400

    Start and pause works!

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,20 +1,66 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
+import java.util.Timer
+import java.util.logging.Handler
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
+
+    //Interact with the Timer
+    //
+
+    lateinit var timerBinder: TimerService.TimerBinder
+    var isConnected = false
+
+
+    val serviceConnection = object : ServiceConnection{
+        override fun onServiceConnected(
+            name: ComponentName?,
+            service: IBinder?
+        ) {
+            timerBinder =  service as TimerService.TimerBinder
+            isConnected = true
+
+        }
+
+        override fun onServiceDisconnected(p0: ComponentName?) {
+            isConnected = false
+        }
+
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        bindService(
+            Intent(this, TimerService::class.java),
+            serviceConnection,
+            BIND_AUTO_CREATE
+        )
+
         findViewById<Button>(R.id.startButton).setOnClickListener {
+            if (isConnected){
+                timerBinder.start(30)
+
+
+            }
 
         }
-        
+        //bind and unbind a service
         findViewById<Button>(R.id.stopButton).setOnClickListener {
+            if(isConnected){
+                timerBinder.pause()
+            }
 
         }
     }
